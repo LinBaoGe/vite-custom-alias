971f8ab20110eaecf42f91c5e92d1f9e75a66d1b
LinBaoGe <1375322655@qq.com>
2022-10-13 00:47:26 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vite-custom-alias
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vite-custom-alias-other-than-reproduction-lgwche)
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'url';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./', import.meta.url)),
+      },
+      {
+        find: 'Src',
+        replacement: fileURLToPath(new URL('./src/aeests', import.meta.url)),
+      },
+    ],
+  },
+});
